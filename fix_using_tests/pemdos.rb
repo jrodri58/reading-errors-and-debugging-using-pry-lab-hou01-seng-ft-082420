10adb00193f6a03faafc01578ab9839ff43044c7
Jassiel Rodriguez <hazer94@gmail.com>
2020-08-02 04:48:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    10 * "s".to_i + string.to_i
   else
     string
   end
